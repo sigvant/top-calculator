1feec950de2c804bdaf86e693fcc345655b4c5d1
sigvant <bnsviv@gmail.com>
2021-07-28 11:11:12 -0300

    add operate function logic which takes operator and calls operation

--- script.js
+++ script.js
@@ -13,3 +13,22 @@ function multiply(a, b) {
 function divide(a, b) {
     return (a / b).toFixed(4);
 }
+
+function operate(operator, a, b) {
+    switch(operator) {
+        case 'addition':
+            add(a, b);
+            break;
+        case 'subtraction':
+            subtract(a, b);
+            break;
+        case 'multiplication':
+            multiply(a, b);
+            break;
+        case 'division':
+            divide(a, b);
+            break;
+        default:
+            console.log('oops');
+    }
+}
